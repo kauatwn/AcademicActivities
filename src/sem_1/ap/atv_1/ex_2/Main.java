b04930985a7ef8f950df6fdb26d3b3a6145e14be
kauatwn <kaualopescontato@gmail.com>
2024-08-29 20:02:13 -0300

    Atividade 1 - Exercício 2

---
+++ src/sem_1/ap/atv_1/ex_2/Main.java
@@ -0,0 +1,20 @@
+package sem_1.ap.atv_1.ex_2;
+
+/* Faça um algoritmo para imprimir em cada linha:
+    - A sua idade;
+    - A sua altura;
+    - O seu status na aula:
+        - True
+        - False */
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        double height = 1.73;
+        boolean status = true;
+
+        System.out.println("Idade: " + age);
+        System.out.println("Altura: " + height);
+        System.out.println("Status do aluno: " + status);
+    }
+}

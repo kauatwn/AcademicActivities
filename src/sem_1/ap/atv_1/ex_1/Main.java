8481370b882d0b53333295a28a7d8821731dd2d6
kauatwn <kaualopescontato@gmail.com>
2024-08-29 20:01:45 -0300

    Atividade 1 - Exercício 1

---
+++ src/sem_1/ap/atv_1/ex_1/Main.java
@@ -0,0 +1,18 @@
+package sem_1.ap.atv_1.ex_1;
+
+/* Faça um algoritmo para imprimir em cada linha:
+    - O seu nome completo;
+    - O seu RA;
+    - O seu curso. */
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Kauã Lima";
+        int ra = 12345;
+        String course = "Análise e Desenvolvimento de Sistemas";
+
+        System.out.println("Nome: " + name);
+        System.out.println("RA: " + ra);
+        System.out.println("Curso: " + course);
+    }
+}

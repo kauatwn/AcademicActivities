ad626c083e8efe5f83a7d655cbff58654dac2c97
kauatwn <kaualopescontato@gmail.com>
2024-08-29 20:02:38 -0300

    Atividade 1 - Exercício 8

---
+++ src/sem_1/ap/atv_1/ex_8/Main.java
@@ -0,0 +1,12 @@
+package sem_1.ap.atv_1.ex_8;
+
+/* Faça um algoritmo para apresentar:
+    - O cálculo da média das seguintes notas 8.0, 7.5, 4.5 e 9.*/
+
+public class Main {
+    public static void main(String[] args) {
+        double result = (8.0 + 7.5 + 4.5 + 9) / 4;
+
+        System.out.print(result);
+    }
+}

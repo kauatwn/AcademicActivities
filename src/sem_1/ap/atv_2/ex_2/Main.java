d50a83b3d981020e0fe30e45a4018c3d81d25a0c
kauatwn <kaualopescontato@gmail.com>
2024-08-30 16:07:04 -0300

    Atividade 2 - Exercício 2

---
+++ src/sem_1/ap/atv_2/ex_2/Main.java
@@ -0,0 +1,59 @@
+package sem_1.ap.atv_2.ex_2;
+
+/* Faça um algoritmo que leia um inteiro entre 1 e 12;
+    – Imprima o nome do mês por extenso. */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número de 1 a 12: ");
+        int number = sc.nextInt();
+
+        switch (number) {
+            case 1:
+                System.out.println("Janeiro");
+                break;
+            case 2:
+                System.out.println("Fevereiro");
+                break;
+            case 3:
+                System.out.println("Março");
+                break;
+            case 4:
+                System.out.println("Abril");
+                break;
+            case 5:
+                System.out.println("Maio");
+                break;
+            case 6:
+                System.out.println("Junho");
+                break;
+            case 7:
+                System.out.println("Julho");
+                break;
+            case 8:
+                System.out.println("Agosto");
+                break;
+            case 9:
+                System.out.println("Setembro");
+                break;
+            case 10:
+                System.out.println("Outubro");
+                break;
+            case 11:
+                System.out.println("Novembro");
+                break;
+            case 12:
+                System.out.println("Dezembro");
+                break;
+            default:
+                System.out.println("Por favor, digite um valor válido");
+                sc.close();
+        }
+
+        sc.close();
+    }
+}

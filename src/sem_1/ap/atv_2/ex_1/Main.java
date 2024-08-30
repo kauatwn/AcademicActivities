0647c67c4ca570ba265cb8b292020b4310a73bcd
kauatwn <kaualopescontato@gmail.com>
2024-08-30 16:06:57 -0300

    Atividade 2 - Exercício 1

---
+++ src/sem_1/ap/atv_2/ex_1/Main.java
@@ -0,0 +1,45 @@
+package sem_1.ap.atv_2.ex_1;
+
+/* Crie um algoritmo que leia um número de 1 a 7.
+    - Imprima o dia da semana correspondente;
+    - Assuma que 1 corresponde ao Domingo. */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número de 1 a 7: ");
+        int number = sc.nextInt();
+
+        switch (number) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
+            case 3:
+                System.out.println("Teça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Por favor, digite um valor válido");
+                sc.close();
+        }
+
+        sc.close();
+    }
+}

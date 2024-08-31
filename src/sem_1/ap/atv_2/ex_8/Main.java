0a19e07de6a6e1889c23e6097d7448748d3a0dc0
kauatwn <kaualopescontato@gmail.com>
2024-08-31 20:27:14 -0300

    Atividade 2 - Exercício 8

---
+++ src/sem_1/ap/atv_2/ex_8/Main.java
@@ -0,0 +1,29 @@
+package sem_1.ap.atv_2.ex_8;
+
+/* Faça um algoritmo que verifique a validade de uma senha 	fornecida pelo usuário.
+    - Sabendo que a senha é R10p5:
+        - Imprimir mensagem de "acesso concedido" ou "acesso negado".
+
+Para comparar duas Strings utilizar o método equals(). Ele retorna um valor booleano. Exemplo:
+    - if(senha.equals("R10p5")) */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String correctPassword = "R10p5";
+
+        System.out.print("Digite a senha: ");
+        String inputPassword = sc.nextLine();
+
+        if (inputPassword.equals(correctPassword)) {
+            System.out.println("Acesso concedido");
+        } else {
+            System.out.println("Acesso negado");
+        }
+
+        sc.close();
+    }
+}

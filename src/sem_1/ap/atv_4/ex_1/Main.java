a47a42fadf625b8060a44d67e583d8f00a9bd9ce
kauatwn <kaualopescontato@gmail.com>
2024-09-08 12:38:19 -0300

    Atividade 4 - Exercício 1

---
+++ src/sem_1/ap/atv_4/ex_1/Main.java
@@ -0,0 +1,14 @@
+package sem_1.ap.atv_4.ex_1;
+
+// Escreva um algoritmo que imprima a sequência do número 100 ao 200.
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 100;
+
+        while (number <= 200){
+            System.out.println(number);
+            number++;
+        }
+    }
+}

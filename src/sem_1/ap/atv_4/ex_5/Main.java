2f7f0e7fcc2809d45256040b1719f0b396c397d8
kauatwn <kaualopescontato@gmail.com>
2024-09-08 12:38:54 -0300

    Atividade 4 - Exercício 5

---
+++ src/sem_1/ap/atv_4/ex_5/Main.java
@@ -0,0 +1,17 @@
+package sem_1.ap.atv_4.ex_5;
+
+// Faça um algoritmo que imprima a tabuada do 5.
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 5;
+        int multiplier = 1;
+
+        while (multiplier <= 10) {
+            int result = number * multiplier;
+
+            System.out.println(number + " x " + multiplier + " = " + result);
+            multiplier++;
+        }
+    }
+}

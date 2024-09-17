e122e333c9cab7527645c8829bc04f6365f2a7f8
kauatwn <kaualopescontato@gmail.com>
2024-09-17 15:14:43 -0300

    Atividade 5 - Exercício 1

---
+++ src/sem_1/ap/atv_5/ex_1/Main.java
@@ -0,0 +1,12 @@
+package sem_1.ap.atv_5.ex_1;
+
+/* Faça um programa que imprima a seguinte sequência:
+    – 100, 99, 98, ..., 1. */
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}

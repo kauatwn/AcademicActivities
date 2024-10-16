259cc9845bb13cda06bc01ce7d885f3f37b8274f
kauatwn <kaualopescontato@gmail.com>
2024-10-15 21:52:31 -0300

    Atividade 7 - Exercício 1

---
+++ src/sem_1/ap/atv_7/ex_1/Main.java
@@ -0,0 +1,20 @@
+package sem_1.ap.atv_7.ex_1;
+
+/* Faça uma função sem retorno chamada mensagem().
+    - Sua função deve imprimir a seguinte mensagem:
+
+        "Meu primeiro programa com funções".
+
+    - Chame a sua função três vezes de dentro do método main. */
+
+public class Main {
+    public static void main(String[] args) {
+        mensagem();
+        mensagem();
+        mensagem();
+    }
+
+    public static void mensagem() {
+        System.out.println("Meu primeiro programa com funções");
+    }
+}

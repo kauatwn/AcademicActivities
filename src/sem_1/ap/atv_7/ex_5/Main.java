4469f94c8bd30ced3daf62c6f8b8cf01381a07c1
kauatwn <kaualopescontato@gmail.com>
2024-10-17 11:23:26 -0300

    Atividade 7 - Exercício 5

---
+++ src/sem_1/ap/atv_7/ex_5/Main.java
@@ -0,0 +1,36 @@
+package sem_1.ap.atv_7.ex_5;
+
+/* Escreva o método exibirDiaSemana:
+    - Deve receber um número inteiro no intervalo de 1 - 7 como parâmetro.
+    - Deve retornar uma String com o dia da semana correspondente aquele número.
+
+Caso seja passado um número fora do intervalo de 1-7:
+    - Retorne uma String com a mensagem: "Dia da semana inválido".
+
+Obs.: Uutilize o switch case dentro do seu método. */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(exibirDiaSemana(1));
+        System.out.println(exibirDiaSemana(2));
+        System.out.println(exibirDiaSemana(3));
+        System.out.println(exibirDiaSemana(4));
+        System.out.println(exibirDiaSemana(5));
+        System.out.println(exibirDiaSemana(6));
+        System.out.println(exibirDiaSemana(7));
+        System.out.println(exibirDiaSemana(8));
+    }
+
+    public static String exibirDiaSemana(int dia) {
+        return switch (dia) {
+            case 1 -> "Domingo";
+            case 2 -> "Segunda-feira";
+            case 3 -> "Terça-feira";
+            case 4 -> "Quarta-feira";
+            case 5 -> "Quinta-feira";
+            case 6 -> "Sexta-feira";
+            case 7 -> "Sábado";
+            default -> "Dia da semana inválido";
+        };
+    }
+}

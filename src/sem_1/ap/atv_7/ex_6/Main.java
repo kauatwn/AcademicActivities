9ed38daeaf508155d76f4ee812df6604afd7776d
kauatwn <kaualopescontato@gmail.com>
2024-10-18 15:19:03 -0300

    Atividade 7 - Exercício 6

--- src/sem_1/ap/atv_7/ex_6/Main.java
+++ src/sem_1/ap/atv_7/ex_6/Main.java
@@ -18,54 +18,54 @@ public class Main {
 
         System.out.print("Digite o tamanho do vetor: ");
         int length = sc.nextInt();
-        int[] vetor = new int[length];
+        int[] array = new int[length];
 
         System.out.println();
         for (int i = 0; i < length; i++) {
             System.out.printf("Digite o valor da posição %d do vetor: ", i);
-            vetor[i] = sc.nextInt();
+            array[i] = sc.nextInt();
         }
         sc.close();
         System.out.println();
 
-        int maiorValor = obterMaiorValor(vetor);
-        System.out.printf("O maior valor do vetor é: %d", maiorValor);
+        int maxValue = getMaxValue(array);
+        System.out.printf("O maior valor do vetor é: %d", maxValue);
         System.out.println();
 
-        int menorValor = obterMenorValor(vetor);
-        System.out.printf("O menor valor do vetor é: %d", menorValor);
+        int minValue = getMinValue(array);
+        System.out.printf("O menor valor do vetor é: %d", minValue);
         System.out.println();
 
-        double media = calcularMedia(vetor);
+        double media = calculateAverage(array);
         System.out.printf("A média do maior e menor valor do vetor é: %.2f", media);
         System.out.println();
     }
 
-    public static int obterMaiorValor(int[] vetor) {
-        int maior = 0;
+    public static int getMaxValue(int[] array) {
+        int maxValue = array[0];
 
-        for (int i = 1; i < vetor.length; i++) {
-            if (vetor[i] > maior) {
-                maior = vetor[i];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
             }
         }
 
-        return maior;
+        return maxValue;
     }
 
-    public static int obterMenorValor(int[] vetor) {
-        int menor = 0;
+    public static int getMinValue(int[] array) {
+        int minValue = array[0];
 
-        for (int i = 1; i < vetor.length; i++) {
-            if (vetor[i] < menor) {
-                menor = vetor[i];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
             }
         }
 
-        return menor;
+        return minValue;
     }
 
-    public static double calcularMedia(int[] vetor) {
-        return (double) (obterMaiorValor(vetor) + obterMenorValor(vetor)) / 2;
+    public static double calculateAverage(int[] array) {
+        return (double) (getMaxValue(array) + getMinValue(array)) / 2;
     }
 }

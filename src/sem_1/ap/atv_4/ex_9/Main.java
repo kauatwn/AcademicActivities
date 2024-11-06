1e22d1be802c6fccbf154f133f9b29a3cba2909d
kauatwn <kaualopescontato@gmail.com>
2024-11-06 11:01:39 -0300

    Atividade 4 - Exercício 9

--- src/sem_1/ap/atv_4/ex_9/Main.java
+++ src/sem_1/ap/atv_4/ex_9/Main.java
@@ -21,16 +21,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double hotDogPrice = 1.20;
-        double simpleBauruPrice = 1.30;
-        double bauruWithEggPrice = 1.50;
-        double hamburgerPrice = 1.20;
-        double cheeseburgerPrice = 1.30;
-        double sodaPrice = 1.00;
+        final double HOT_DOG_PRICE = 1.20;
+        final double SIMPLE_BAURU_PRICE = 1.30;
+        final double BAURU_WITH_EGG_PRICE = 1.50;
+        final double HAMBURGER_PRICE = 1.20;
+        final double CHEESEBURGER_PRICE = 1.30;
+        final double SODA_PRICE = 1.00;
 
         boolean continueBuying = true;
         double totalValue = 0.0;
-        double productValue = 0.0;
 
         do {
             System.out.println("""
@@ -43,45 +42,40 @@ public class Main {
                     | 104    | Cheeseburger      | R$ 1,30  |
                     | 105    | Refrigerante      | R$ 1,00  |
                     """);
+
             System.out.print("Digite o código do produto: ");
             int productCode = sc.nextInt();
 
             System.out.print("Digite a quantidade: ");
             int quantity = sc.nextInt();
 
-            switch (productCode) {
-                case 100:
-                    productValue = hotDogPrice * quantity;
-                    break;
-                case 101:
-                    productValue = simpleBauruPrice * quantity;
-                    break;
-                case 102:
-                    productValue = bauruWithEggPrice * quantity;
-                    break;
-                case 103:
-                    productValue = hamburgerPrice * quantity;
-                    break;
-                case 104:
-                    productValue = cheeseburgerPrice * quantity;
-                    break;
-                case 105:
-                    productValue = sodaPrice * quantity;
-                    break;
-                default:
-                    System.out.println("Código inválido.");
-            }
+            double productValue = switch (productCode) {
+                case 100 -> HOT_DOG_PRICE * quantity;
+                case 101 -> SIMPLE_BAURU_PRICE * quantity;
+                case 102 -> BAURU_WITH_EGG_PRICE * quantity;
+                case 103 -> HAMBURGER_PRICE * quantity;
+                case 104 -> CHEESEBURGER_PRICE * quantity;
+                case 105 -> SODA_PRICE * quantity;
+                default -> {
+                    System.out.println("Código inválido. Por favor, digite um código válido.");
+                    yield 0.0;
+                }
+            };
 
             totalValue += productValue;
+            System.out.println();
+
             System.out.print("Deseja continuar comprando (S/N)? ");
             char answer = sc.next().toUpperCase().charAt(0);
+            System.out.println();
 
             if (answer != 'S') {
                 continueBuying = false;
             }
         } while (continueBuying);
 
-        System.out.println("Valor total a pagar: R$ " + totalValue);
         sc.close();
+
+        System.out.printf("Valor total a pagar: R$ %.2f", totalValue);
     }
 }

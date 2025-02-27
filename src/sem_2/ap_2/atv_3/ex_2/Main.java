1e0731c171e6de1264bd6259b52c4254a176299e
kauatwn <kaualopescontato@gmail.com>
2025-02-27 15:33:51 -0300

    Atividade 3 - Exercício 2

---
+++ src/sem_2/ap_2/atv_3/ex_2/Main.java
@@ -0,0 +1,84 @@
+package sem_2.ap_2.atv_3.ex_2;
+
+import java.util.Random;
+
+// No mesmo programa do exercício anterior, crie um método para realizar a busca binária e compare a quantidade de
+// repetições necessárias em cada um dos métodos. Compare também o tempo gasto.
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[100_000];
+        fillArray(array);
+
+        Random random = new Random();
+        int value = random.nextInt(0, 100_000);
+
+        // Busca linear -> O(n)
+        long start = System.currentTimeMillis();
+        int foundPosition = findPosition(array, value);
+        long end = System.currentTimeMillis();
+        long elapsedTime = end - start;
+
+        System.out.printf("Valor %d encontrado na posição %d%n", value, foundPosition);
+        System.out.printf("Tempo de execução: %dms%n", elapsedTime);
+
+        System.out.println();
+
+        // Busca binária -> O(log n)
+        start = System.currentTimeMillis();
+        int binarySearchPosition = binarySearch(array, value);
+        end = System.currentTimeMillis();
+        elapsedTime = end - start;
+
+        System.out.printf("Valor %d encontrado na posição %d%n", value, binarySearchPosition);
+        System.out.printf("Tempo de execução: %dms%n", elapsedTime);
+    }
+
+    private static void fillArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+    }
+
+    private static int findPosition(int[] array, int target) {
+        int position = -1;
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            count++;
+            if (array[i] == target) {
+                System.out.printf("Repetições na Busca Linear: %d%n", count);
+                return i;
+            }
+        }
+
+        System.out.printf("Repetições na Busca Linear: %d%n", count);
+        return position;
+    }
+
+    private static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        int count = 0;
+
+        while (left <= right) {
+            count++;
+            // // Chance de overflow
+            // int middle = (left + right) / 2;
+
+            // Evita overflow
+            int middle = left + (right - left) / 2;
+
+            if (array[middle] == target) {
+                System.out.printf("Repetições na Busca Binária: %d%n", count);
+                return middle;
+            } else if (array[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        System.out.printf("Repetições na Busca Binária: %d%n", count);
+        return -1;
+    }
+}

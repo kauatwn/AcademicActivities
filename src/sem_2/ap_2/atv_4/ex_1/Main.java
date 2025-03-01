b450fe355ddb6f523b10357667ed3e3f3a0fc230
kauatwn <kaualopescontato@gmail.com>
2025-02-28 21:46:31 -0300

    Refatora código para preencher o array e verificar valor aleatório

--- src/sem_2/ap_2/atv_4/ex_1/Main.java
+++ src/sem_2/ap_2/atv_4/ex_1/Main.java
@@ -1,15 +1,29 @@
 package sem_2.ap_2.atv_4.ex_1;
 
+import java.util.Random;
+
 // Crie um método que verifica se um array possui um determinado valor.
 public class Main {
     public static void main(String[] args) {
-        int[] array = { 1, 2, 3, 4, 5 };
-        int value = 3;
+        Random random = new Random();
+
+        int[] array = new int[10];
+        fillArray(array);
+
+        int value = random.nextInt(100);
 
         if (hasValue(array, value)) {
-            System.out.printf("O array possui o valor %d", value);
+            System.out.printf("O array possui o valor '%d'", value);
         } else {
-            System.out.printf("O array não possui o valor %d", value);
+            System.out.printf("O array não possui o valor '%d'", value);
+        }
+    }
+
+    private static void fillArray(int[] array) {
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
         }
     }
 
